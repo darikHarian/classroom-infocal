2a8174bfc577f7f27d7c54400c7d52bfb55b8a36
Darik Harian Khal <contacto@alphacorp.cl>
2024-05-20 11:41:05 -0400

    Agregando archivos base desafío guiado del módulo 4, sección 3, dia 2

---
+++ modulo4/seccion3/dia2/desafioOpcional/index.js
@@ -0,0 +1,4 @@
+import {radiologia, traumatologia, dental} from './registros.js'
+console.log(radiologia)
+console.log(traumatologia)
+console.log(dental)
---
+++ modulo4/seccion3/dia2/desafioOpcional/registros.js
@@ -0,0 +1,88 @@
+export let radiologia = [
+    {
+        'hora': '11:00',
+        'especialista': 'Ignacio Schulz',
+        'paciente': 'Francisca Rojas',
+        'rut': '9878782-1',
+        'prevision': 'FONASA'
+    },
+    {
+        'hora': '11:30',
+        'especialista': 'Federico Subercaseaux',
+        'paciente': 'Pamela Estrada',
+        'rut': '15345241-3',
+        'prevision': 'ISAPRE'
+    }, 
+    {
+        'hora': '15:00',
+        'especialista': 'Fernando Wurthz',
+        'paciente': 'Armando Luna',
+        'rut': '16445345-9',
+        'prevision': 'ISAPRE'
+    },
+    {
+        'hora': '15:30',
+        'especialista': 'Ana María Godoy',
+        'paciente': 'Manuel Godoy',
+        'rut': '17666419-0',
+        'prevision': 'FONASA'
+    },
+    {
+        'hora': '16:00',
+        'especialista': 'Patricia Suazo',
+        'paciente': 'Ramón Ulloa',
+        'rut': '14989389-K',
+        'prevision': 'FONASA'
+    }
+];
+
+export let traumatologia = [
+    {
+        'hora': '08:00',
+        'especialista': 'María Paz Altuzarra',
+        'paciente': 'Paula Sánchez',
+        'rut': '15554774-5',
+        'prevision': 'FONASA'
+    },
+    {
+        'hora': '10:00',
+        'especialista': 'Raúl Araya',
+        'paciente': 'Angélica Navas',
+        'rut': '15444147-9',
+        'prevision': 'ISAPRE'
+    },
+    {
+        'hora': '10:30',
+        'especialista': 'María Arriagada',
+        'paciente': 'Ana Klapp',
+        'rut': '17879423-9',
+        'prevision': 'ISAPRE'
+    },
+    {
+        'hora': '11:00',
+        'especialista': 'Alejandro Badilla',
+        'paciente': 'Felipe Mardones',
+        'rut': '1547423-6',
+        'prevision': 'ISAPRE'
+    },
+    {
+        'hora': '11:30',
+        'especialista': 'Cecilia Budnik',
+        'paciente': 'Diego Marre',
+        'rut': '16554741-k',
+        'prevision': 'FONASA'
+    },
+    {
+        'hora': '10:30',
+        'especialista': 'María Arriagada',
+        'paciente': 'Ana Klapp',
+        'rut': '17879423-9',
+        'prevision': 'ISAPRE'
+    }
+];
+
+export let dental = [
+    {
+
+    }
+];
